9a305e3c1db320357d9f2d3521c57e283f1dd950
tim76 <tim761452@gmail.com>
2025-04-12 04:18:29 +0300

    w2

--- auth_service/config.py
+++ auth_service/config.py
@@ -1,6 +1,6 @@
 import os
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-SQLALCHEMY_DATABASE_URI = 'postgresql://admin:844622@localhost:5432/mkrf'
+SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:844622@localhost:5432/mkrf'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = "verysecretkey123"
