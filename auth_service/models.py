81e37c69ca1a2df685bdc8e69cab4923e9da7d51
tim76 <tim761452@gmail.com>
2025-04-12 05:14:05 +0300

    w5

--- auth_service/models.py
+++ auth_service/models.py
@@ -1,13 +1,14 @@
 from common.db import db
 from werkzeug.security import generate_password_hash, check_password_hash
 
-
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, nullable=False)
-    # Поле email удалено, так как не требуется
+    email = db.Column(db.String(120), unique=True, nullable=False, default='')
     password_hash = db.Column(db.String(128))
     max_rebirths = db.Column(db.Integer, default=0)
+
+
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
---
+++ check_db_connection.py
@@ -0,0 +1,3 @@
+# check_db_connection.py
+
+from common import db  # Импортируйте объект db из вашего приложения
